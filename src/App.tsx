abbc5ad4226586c6627871238ece00e83fba28b3
Visweshvar-UST <287618@ust.com>
2024-11-02 06:35:06 +0530

    object handling problem

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,14 @@ function App() {
     )();
   },[]);
 
+  const res = data.reduce((max, obj)=> (max.amount<obj.amount)?obj:max,data[0]);
+
 return (
     <div className="App">
       <h1>total transaction</h1> {data.reduce((acc, data)=> (acc+data.amount),0)}
       <h1>total transaction in cash</h1> {data.filter((obj) => obj.cash).reduce((acc, data) => (acc + data.amount), 0)}
       <h1>total transaction in credit</h1> {data.filter((obj) => !obj.cash).reduce((acc, data) => (acc + data.amount), 0)}
-      <h1>maximum transaction</h1> {data.reduce((max, data)=> (max<data.amount)?data.amount:max,Number.MIN_VALUE)}
+      <h1>maximum transaction</h1> {res && (<p>{res.name}{res.amount}</p>)}
     </div>
   );
 }
