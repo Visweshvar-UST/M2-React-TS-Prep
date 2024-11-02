6e4affff0139c4c63a1ba28d5ca32c040e0307ff
Visweshvar-UST <287618@ust.com>
2024-11-02 06:12:43 +0530

    tot done with type W .data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+interface Sales {
+  name: string,
+  amount: number,
+  cash: boolean
+}
+
+
 function App() {
-  return (
+
+  const [data , setdata] = useState<Sales[]>([]);
+  
+  useEffect(()=>{
+    (
+      async ()=>{
+        const result = await axios.get<Sales[]>('./sales.json');
+        console.log(result);
+        
+        setdata(result.data);
+      }
+    )();
+  },[]);
+
+return (
     <div className="App">
-      <h1>total transaction</h1>
+      <h1>total transaction</h1> {data.reduce((acc, data)=> (acc+data.amount),0)}
+      <h1>total transaction in cash</h1> {data.filter((obj) => obj.cash).reduce((acc, data) => (acc + data.amount), 0)}
     </div>
   );
 }
